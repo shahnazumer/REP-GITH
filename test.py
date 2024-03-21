92aa28cfaa928e568f36ff776636bdbd810d4e8f
Shahnaz Umer <106480938+shahnazumer@users.noreply.github.com>
2024-03-21 17:01:14 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,5 @@
-import pytest
-from fastapi.testclient import TestClient
-from main import app  # assuming main is the module where your FastAPI app is defined
-
-# Create a TestClient instance
-client = TestClient(app)
+from app import index
 
 
-# Define tests
-def test_read_root():
-    # Make a GET request to the /test endpoint
-    response = client.get("/test")
-    
-    # Assert that the status code is 200 OK
-    assert response.status_code == 200
-    
-    # Assert that the response body contains the expected data
-    assert response.json() == {"Hello": "World"}
+def test_index():
+    assert index() == "HELLO, WORLD!"
